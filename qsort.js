ee084fcb600fc11f5e5a8b349d2c422eb6300f87
blizzardKv <makemewanna@yandex.ru>
2021-09-10 18:52:45 +0300

    fix equal numbers values sorting, #1

--- qsort.js
+++ qsort.js
@@ -1,10 +1,16 @@
+const array = [1, 2, 3, 1231, 1231, 123, 1, 3, 421321, 123];
+
 const qsort = (arr) => {
     if (arr.length < 2 ) {
         return arr;
-    } else {
-        const pivot = Math.floor(Math.random() * arr.length);
-        const less = arr.filter((item) => item < pivot);
-        const greater = arr.filter((item) => item > pivot);
-        return [...qsort(less), pivot, ...qsort(greater)];
     }
+
+    const pivotPosition =  Math.floor(Math.random() * arr.length);
+    const pivot = arr[pivotPosition];
+    const less = arr.filter((item) => item < pivot);
+    const greater = arr.filter((item) => item > pivot);
+    const equal = arr.filter((item) => item === pivot);
+    return [...qsort(less), ...equal, ...qsort(greater)];
 }
+
+console.log(qsort(array));
