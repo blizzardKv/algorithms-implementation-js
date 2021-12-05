4a604abbf0b2e533504332af5cae32168be0c2a5
blizzardKv <makemewanna@yandex.ru>
2021-12-05 20:18:42 +0300

    refactoring, #1

--- binary.js
+++ binary.js
@@ -1,27 +1,27 @@
 const array = [1, 231, 12312, 12314, 12, 312, 312312, 4444];
 
 const sortedArr = (arr) => {
-    return arr.sort((a, b) => a - b);
+  return arr.sort((a, b) => a - b);
 }
 
 const binary = (arr, item) => {
-    let lowElIndex = 0;
-    let highElIndex = arr.length - 1;
+  let lowElIndex = 0;
+  let highElIndex = arr.length - 1;
 
-    while (lowElIndex <= highElIndex) {
-        const midElIndex = Math.floor((lowElIndex + highElIndex) / 2);
-        const guessEl = arr[midElIndex];
+  while (lowElIndex <= highElIndex) {
+    const midElIndex = Math.floor((lowElIndex + highElIndex) / 2);
+    const guessEl = arr[midElIndex];
 
-        if (guessEl === item) return guessEl;
+    if (guessEl === item) return guessEl;
 
-        if (guessEl > item) {
-            highElIndex = midElIndex - 1;
-        } else {
-            lowElIndex = midElIndex + 1;
-        }
+    if (guessEl > item) {
+      highElIndex = midElIndex - 1;
+    } else {
+      lowElIndex = midElIndex + 1;
     }
+  }
 
-    return 'Element is not found';
+  return 'Element is not found';
 }
 
-console.log(binary(sortedArr(array), 12));
+console.log(binary(sortedArr(array), 312));
